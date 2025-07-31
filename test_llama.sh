eefd68760e7884a091886883a2708ee58e126b56
Duong Nguyen <duong.nkt@namitech.io>
2025-07-31 09:43:58 +0700

    fix: add --test-local to bash files

--- test_llama.sh
+++ test_llama.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python benchmark.py --transcript-file different_10_batches.json  --inference-type "gguf" --request-handle "parallel" 
+python benchmark.py --transcript-file different_10_batches.json  --inference-type "gguf" --request-handle "parallel" --test-local
--- test_vllm.sh
+++ test_vllm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python benchmark.py --transcript-file different_10_batches.json  --inference-type "vllm" --request-handle "parallel" --request-handle "parallel"
+python benchmark.py --transcript-file different_10_batches.json  --inference-type "vllm" --request-handle "parallel" --test-local
